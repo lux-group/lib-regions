dbffd8c0a8c76cf1cab7cb5102319f7d706e2a1e
Rufus Post <rufuspost@gmail.com>
2017-10-19 17:21:06 +1100

    add phone numbers (#11)

--- region-data.js
+++ region-data.js
@@ -6,6 +6,16 @@ module.exports = {
       lang: 'en-AU',
       currency_formatting_locale: 'en-AU',
       currency_code: 'AUD',
+      phone: {
+        local: {
+          human_readable: '1300 88 99 00',
+          number: '1300889900',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'CA',
@@ -13,6 +23,16 @@ module.exports = {
       lang: 'en-CA',
       currency_formatting_locale: 'en-CA',
       currency_code: 'CAD',
+      phone: {
+        local: {
+          human_readable: '1 (650) 887 7712.',
+          number: '16508877712.',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'FR',
@@ -20,6 +40,16 @@ module.exports = {
       lang: 'en-FR',
       currency_formatting_locale: 'fr-FR',
       currency_code: 'EUR',
+      phone: {
+        local: {
+          human_readable: '1300 88 99 00',
+          number: '1300889900',
+        },
+        international: {
+          human_readable: '+44 (203) 695 0392',
+          number: '+442036950392',
+        },
+      },
     },
     {
       code: 'DE',
@@ -27,6 +57,16 @@ module.exports = {
       lang: 'en-DE',
       currency_formatting_locale: 'de-DE',
       currency_code: 'EUR',
+      phone: {
+        local: {
+          human_readable: '1300 88 99 00',
+          number: '1300889900',
+        },
+        international: {
+          human_readable: '+44 (203) 695 0392',
+          number: '+442036950392',
+        },
+      },
     },
     {
       code: 'HK',
@@ -34,6 +74,16 @@ module.exports = {
       lang: 'en-HK',
       currency_formatting_locale: 'en-HK',
       currency_code: 'HKD',
+      phone: {
+        local: {
+          human_readable: '800 906 442',
+          number: '800906442',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'IN',
@@ -41,6 +91,16 @@ module.exports = {
       lang: 'en-IN',
       currency_formatting_locale: 'en-IN',
       currency_code: 'INR',
+      phone: {
+        local: {
+          human_readable: '1800 1200 336',
+          number: '18001200336',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'IE',
@@ -48,6 +108,16 @@ module.exports = {
       lang: 'en-IE',
       currency_formatting_locale: 'en-IE',
       currency_code: 'EUR',
+      phone: {
+        local: {
+          human_readable: '1300 88 99 00',
+          number: '1300889900',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'IL',
@@ -55,6 +125,16 @@ module.exports = {
       lang: 'en-IL',
       currency_formatting_locale: 'iw-IL',
       currency_code: 'ILS',
+      phone: {
+        local: {
+          human_readable: '1 801 227 262',
+          number: '1801227262',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'IT',
@@ -62,6 +142,16 @@ module.exports = {
       lang: 'en-IT',
       currency_formatting_locale: 'it-IT',
       currency_code: 'EUR',
+      phone: {
+        local: {
+          human_readable: '1300 88 99 00',
+          number: '1300889900',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'KR',
@@ -69,6 +159,16 @@ module.exports = {
       lang: 'en-KR',
       currency_formatting_locale: 'ko-KR',
       currency_code: 'KRW',
+      phone: {
+        local: {
+          human_readable: '0808 220 277',
+          number: '0808220277',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'MY',
@@ -76,6 +176,16 @@ module.exports = {
       lang: 'en-MY',
       currency_formatting_locale: 'ms-MY',
       currency_code: 'MYR',
+      phone: {
+        local: {
+          human_readable: '1300 88 99 00',
+          number: '1300889900',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'NZ',
@@ -83,6 +193,16 @@ module.exports = {
       lang: 'en-NZ',
       currency_formatting_locale: 'en-NZ',
       currency_code: 'NZD',
+      phone: {
+        local: {
+          human_readable: '0800 441 457',
+          number: '0800441457',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'PH',
@@ -90,6 +210,16 @@ module.exports = {
       lang: 'en-PH',
       currency_formatting_locale: 'en-PH',
       currency_code: 'PHP',
+      phone: {
+        local: {
+          human_readable: '1800 1320 0074',
+          number: '180013200074',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'SG',
@@ -97,6 +227,16 @@ module.exports = {
       lang: 'en-SG',
       currency_formatting_locale: 'en-SG',
       currency_code: 'SGD',
+      phone: {
+        local: {
+          human_readable: '800 492 2237',
+          number: '8004922237',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'ZA',
@@ -104,6 +244,16 @@ module.exports = {
       lang: 'en-ZA',
       currency_formatting_locale: 'af-ZA',
       currency_code: 'ZAR',
+      phone: {
+        local: {
+          human_readable: '0800 060 109',
+          number: '0800060109',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
     {
       code: 'ES',
@@ -111,6 +261,16 @@ module.exports = {
       lang: 'en-ES',
       currency_formatting_locale: 'es-ES',
       currency_code: 'EUR',
+      phone: {
+        local: {
+          human_readable: '1300 88 99 00',
+          number: '1300889900',
+        },
+        international: {
+          human_readable: '+44 (203) 695 0392',
+          number: '+442036950392',
+        },
+      },
     },
     {
       code: 'GB',
@@ -118,6 +278,16 @@ module.exports = {
       lang: 'en-GB',
       currency_formatting_locale: 'en-GB',
       currency_code: 'GBP',
+      phone: {
+        local: {
+          human_readable: '8000 988 722',
+          number: '8000988722',
+        },
+        international: {
+          human_readable: '+44 (203) 695 0392',
+          number: '+442036950392',
+        },
+      },
     },
     {
       code: 'US',
@@ -125,6 +295,16 @@ module.exports = {
       lang: 'en-US',
       currency_formatting_locale: 'en-US',
       currency_code: 'USD',
+      phone: {
+        local: {
+          human_readable: '1800 768 6102',
+          number: '18007686102',
+        },
+        international: {
+          human_readable: '+61 3 90200250',
+          number: '+61390200250',
+        },
+      },
     },
   ],
 
