bcd53477d329c30dda6b2454486d26ff65186b2b
Saidur Rahman <saidur.rahman@luxgroup.com>
2018-08-28 12:25:07 +1000

    Remove Braintree for all NZD

--- currency-data.js
+++ currency-data.js
@@ -4,7 +4,6 @@ module.exports = {
       NZD: {
         payment_methods: [
           'stripe',
-          'braintree',
         ],
       },
     },
@@ -44,7 +43,6 @@ module.exports = {
       NZD: {
         payment_methods: [
           'stripe',
-          'braintree'
         ]
       }
     },
