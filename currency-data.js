3a7bcd66cb146a9a1bf352bf19e184838b732838
Zahra Dargahi <zlotusa@gmail.com>
2019-05-27 14:35:05 +1000

    add giftcard to other regions

--- currency-data.js
+++ currency-data.js
@@ -78,24 +78,28 @@ module.exports = {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       CNY: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       EUR: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       HKD: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       INR: {
@@ -103,120 +107,140 @@ module.exports = {
           'le_credit',
           'stripe',
           'razorpay',
+          'giftcard',
         ],
       },
       IDR: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       ILS: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       JPY: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       KRW: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       MOP: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       MYR: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       NZD: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       PHP: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       QAR: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       RUB: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       SAR: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       SGD: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       TWD: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ]
       },
       THB: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ]
       },
       ZAR: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       AED: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       GBP: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       USD: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
       VND: {
         payment_methods: [
           'le_credit',
           'stripe',
+          'giftcard',
         ],
       },
     },
