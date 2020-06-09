48c1113e304ecca0b2acc8102d725d7cc46f5a39
Joshua Cullen <joshua.cullen@luxgroup.com>
2020-06-10 09:52:00 +1000

    Add AUD currency to zoomzoom (#151)

--- src/regions.ts
+++ src/regions.ts
@@ -1540,5 +1540,32 @@ export const regions: BrandRegions = {
       giftCardOptions: [],
       offerUrgencyTag: null,
     },
+    {
+      code: "AU",
+      name: "Australia",
+      lang: "en-KR",
+      phonePrefix: "82",
+      currencyFormattingLocale: "en-AU",
+      currencyCode: "AUD",
+      currencyPrefix: "A",
+      flagId: "au_iuox02",
+      flightsSupportEmail: DEFAULT_FLIGHTS_SUPPORT_EMAIL,
+      phone: {
+        local: {
+          humanReadable: "+61 2 8387 99837",
+          number: "+61283879983",
+        },
+        international: {
+          humanReadable: "+61 2 8320 6845",
+          number: "+61283879983",
+        },
+        default: "international",
+      },
+      mailingAddress: "Suite 55, 104 Bathurst Street, Sydney, NSW 2000 Australia",
+      referralAmount: 0,
+      referralMinSpendAmount: 0,
+      giftCardOptions: [],
+      offerUrgencyTag: null,
+    },
   ],
 };
