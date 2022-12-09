4e8f32a7b96316d385d59a65ff73857e4686830a
Henry Shen <83993302+henryshen-le@users.noreply.github.com>
2022-12-09 16:14:44 +1100

    remove comments

--- src/extended.ts
+++ src/extended.ts
@@ -1,23 +1,13 @@
 import { currencies, Currency } from "./currencies";
-import {Brand, BRANDS, Region, regions} from "./regions";
+import { BRANDS, Region, regions } from "./regions";
 
 export interface ExtendedRegion extends Region, Currency {}
 
-// interface ExtendedBrandRegions {
-//   [brand: string]: ExtendedRegion[];
-// }
-
 type ExtendedBrandRegions = Record<typeof BRANDS[number], Region[]>;
-
-const extend = (region: Region, currency: Currency) => ({ ...region, ...currency });
 
 const extendedRegions = {} as ExtendedBrandRegions;
 
-// const extendedRegions: ExtendedBrandRegions = Object.keys(regions).reduce((result: ExtendedBrandRegions, key: Brand) => {
-//   result[key] = regions[key].map((region: Region) => (
-//     extend(region, currencies[key][region.currencyCode]);
-//   return result;
-// }, {});
+const extend = (region: Region, currency: Currency) => ({ ...region, ...currency });
 
 for (const brand of BRANDS) {
   extendedRegions[brand] = regions[brand].map((region: Region) => (
