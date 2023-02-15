f2c1fbd3686d5f31d12fecc43db78839a1923d92
Matthew Delaney <107819446+MatthewDelaneyAtLuxuryEscapes@users.noreply.github.com>
2023-02-16 10:21:02 +1100

    LEB-11 add priority phone numbers to leforwork brand (#265)

--- src/priorityPhoneNumbers.ts
+++ src/priorityPhoneNumbers.ts
@@ -135,4 +135,23 @@ export const priorityPhoneNumbers: BrandPriorityPhoneNumber = {
       },
     },
   ],
+  leforwork: [
+    {
+      code: "AU",
+      name: "Australia",
+      lang: "en-AU",
+      phonePrefix: "61",
+      phone: {
+        local: {
+          humanReadable: "1300 86 04 54",
+          number: "1300860454",
+        },
+        international: {
+          humanReadable: "+61 3 8593 4859",
+          number: "+61385934859",
+        },
+        default: "international",
+      },
+    },
+  ],
 };
