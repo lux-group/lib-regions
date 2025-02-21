9168e88a69bfbd306bea393280b097456494c94c
ron-crisostomo <rcrisostomo97@gmail.com>
2025-02-21 11:28:28 +1100

    try min import?

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { CountryCode, isPossiblePhoneNumber } from "libphonenumber-js";
+import { CountryCode, isPossiblePhoneNumber } from "libphonenumber-js/min";
 import { currencies as _currencies } from "./currencies";
 import { ExtendedRegion as Region, extendedRegions } from "./extended";
 import { ALLOWED_CFMR_ORIGIN_COUNTRIES, ALLOWED_ORIGIN_COUNTRIES, COVER_GENIUS_COUNTRIES, UNIVERSAL_COUNTRIES } from "./insurance";
