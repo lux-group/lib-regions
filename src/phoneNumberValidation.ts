f169dabd9310dcfaf8ff93be32c169378530937d
ron-crisostomo <rcrisostomo97@gmail.com>
2025-02-24 12:50:43 +1100

    linting

--- src/phoneNumberValidation.ts
+++ src/phoneNumberValidation.ts
@@ -3,42 +3,42 @@ import { phone } from "phone";
 const NON_NUMERICAL_CHARACTERS_REGEX = /[^0-9]/;
 
 const REGIONS_WITH_ZERO_TRUNK_PREFIX = new Set([
-  'AT', // Austria
-  'AU', // Australia
-  'BD', // Bangladesh
-  'BE', // Belgium
-  'BG', // Bulgaria
-  'CZ', // Czech Republic
-  'DE', // Germany
-  'DZ', // Algeria
-  'EG', // Egypt
-  'FI', // Finland
-  'FR', // France
-  'GB', // United Kingdom
-  'GR', // Greece
-  'ID', // Indonesia
-  'IE', // Ireland
-  'IN', // India
-  'IT', // Italy
-  'JP', // Japan
-  'KE', // Kenya
-  'MA', // Morocco
-  'MY', // Malaysia
-  'NG', // Nigeria
-  'NL', // Netherlands
-  'NZ', // New Zealand
-  'PH', // Philippines
-  'PK', // Pakistan
-  'PL', // Poland
-  'RO', // Romania
-  'SE', // Sweden
-  'CH', // Switzerland
-  'TH', // Thailand
-  'TR', // Turkey
-  'TZ', // Tanzania
-  'UG', // Uganda
-  'VN', // Vietnam
-  'ZA'  // South Africa
+  "AT", // Austria
+  "AU", // Australia
+  "BD", // Bangladesh
+  "BE", // Belgium
+  "BG", // Bulgaria
+  "CZ", // Czech Republic
+  "DE", // Germany
+  "DZ", // Algeria
+  "EG", // Egypt
+  "FI", // Finland
+  "FR", // France
+  "GB", // United Kingdom
+  "GR", // Greece
+  "ID", // Indonesia
+  "IE", // Ireland
+  "IN", // India
+  "IT", // Italy
+  "JP", // Japan
+  "KE", // Kenya
+  "MA", // Morocco
+  "MY", // Malaysia
+  "NG", // Nigeria
+  "NL", // Netherlands
+  "NZ", // New Zealand
+  "PH", // Philippines
+  "PK", // Pakistan
+  "PL", // Poland
+  "RO", // Romania
+  "SE", // Sweden
+  "CH", // Switzerland
+  "TH", // Thailand
+  "TR", // Turkey
+  "TZ", // Tanzania
+  "UG", // Uganda
+  "VN", // Vietnam
+  "ZA",  // South Africa
 ]);
 
 export function isPhoneNumberValidForRegion(
@@ -52,17 +52,17 @@ export function isPhoneNumberValidForRegion(
     // note: this will also capture any cases where there is an international country code attached to
     // the number (e.g. +61)
     if (NON_NUMERICAL_CHARACTERS_REGEX.test(phoneNumber)) {
-      return false
+      return false;
     }
 
-    if(REGIONS_WITH_ZERO_TRUNK_PREFIX.has(regionCode) && phoneNumber.startsWith('0')) {
-      return false
+    if (REGIONS_WITH_ZERO_TRUNK_PREFIX.has(regionCode) && phoneNumber.startsWith("0")) {
+      return false;
     }
 
-    const phoneNumberResult = phone(phoneNumber, { 
+    const phoneNumberResult = phone(phoneNumber, {
       country: regionCode,
-      strictDetection: true
+      strictDetection: true,
     });
 
-    return phoneNumberResult.isValid
+    return phoneNumberResult.isValid;
 }
